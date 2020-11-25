8c194b5f79bce4db6a6e1452164b68e19bc1f5a7
kevinoop0 <kevinoop0@gmail.com>
2020-11-25 15:56:40 +0800

    second_commit

--- model.py
+++ model.py
@@ -105,7 +105,7 @@ class Model(nn.Module):
         super().__init__()
         self.vgg_encoder = VGGEncoder()
         self.decoder = Decoder()
-        self.gcn = GCN(nfeat=512, nhid=256, nclass=512, dropout=0.5)
+        self.gcn = GCN(nfeat=512, nhid=512, nclass=512, dropout=0.3)
 
     def gcn_in(self, cf, sf):
         # [8, 512, 32, 32]
@@ -113,12 +113,13 @@ class Model(nn.Module):
         sf_flaten = sf.reshape(sf.shape[0], -1)
         A = torch.mm(sf_flaten, sf_flaten.T) # bs*bs
         # ipdb.set_trace()
-        A_norm = torch.div(A, A.max())
+        A_norm = torch.div(A, A.max(dim=0)[0])
         sf_mean_new = self.gcn(sf_mean_init, A_norm).reshape(sf.shape[0], sf.shape[1], 1, 1)
         cf_mean, cf_std = calc_mean_std(cf)
         _, sf_std = calc_mean_std(sf)
         # [8, 512, 32, 32]
         normalized_features = sf_std * (cf - cf_mean) / cf_std + sf_mean_new
+        
         return normalized_features
 
     def generate(self, content_images, style_images, alpha=1.0):
@@ -126,7 +127,7 @@ class Model(nn.Module):
         style_features = self.vgg_encoder(style_images, output_last_feature=True)
         # t = adain(content_features, style_features)
         t = self.gcn_in(content_features, style_features)
-        t = alpha * t + (1 - alpha) * content_features
+        # t = alpha * t + (1 - alpha) * content_features
         out = self.decoder(t)
         return out
 
@@ -148,7 +149,7 @@ class Model(nn.Module):
         style_features = self.vgg_encoder(style_images, output_last_feature=True)
         # t = adain(content_features, style_features)
         t = self.gcn_in(content_features, style_features)
-        t = alpha * t + (1 - alpha) * content_features
+        # t = alpha * t + (1 - alpha) * content_features
         out = self.decoder(t)
 
         output_features = self.vgg_encoder(out, output_last_feature=True)
@@ -157,5 +158,5 @@ class Model(nn.Module):
 
         loss_c = self.calc_content_loss(output_features, t)
         loss_s = self.calc_style_loss(output_middle_features, style_middle_features)
-        loss = loss_c + lam * loss_s
-        return loss, out
+        # loss = loss_c + lam * loss_s
+        return loss_c, loss_s, out
--- train.py
+++ train.py
@@ -14,12 +14,12 @@ import numpy as np
 
 
 class Config(object):
-    epoch = 20
+    epoch = 15
     lr = 5e-5
-    batch_size = 64
+    batch_size = 16
     gpu_id = 'cuda:0'
-    loss_interval = 20
-    img_interval = 50
+    loss_interval = 10
+    img_interval = 500
     test_bs = 3
 
 
@@ -29,7 +29,7 @@ def train(**kwargs):
         setattr(opt, k, v)
 
     device = torch.device(opt.gpu_id)
-    vis = visdom.Visdom(env='train')  # python -m visdom.server
+    vis = visdom.Visdom(env='gin')  # python -m visdom.server
 
     train_dataset = PreprocessDataset(
         '/data/lzd/train_data/content', '/data/lzd/train_data/style')
@@ -44,41 +44,57 @@ def train(**kwargs):
 
     iters = len(train_dataset)
     print(f'Length of train image pairs: {iters}')
-    model = Model()
-    model= torch.nn.DataParallel(model, device_ids=[0,1,2,3])
-    model.to(device)
-    optimizer = Adam(model.parameters(), lr=opt.lr)
+    # model = Model()
+    # model= torch.nn.DataParallel(model, device_ids=[0,1,2,3])
+    # model.to(device)
+    model = Model().to(device)
+    optimizer = Adam([
+        {'params': model.decoder.parameters(), 'lr': opt.lr},
+        {'params': model.gcn.parameters(), 'lr': opt.lr*10}], lr=opt.lr)
 
     for e in range(1, opt.epoch):
         print(f'start {e} epoch:')
         for i, (content, style) in enumerate(train_loader, 1):
             content = content.to(device)  # [8, 3, 256, 256]
             style = style.to(device)  # [8, 3, 256, 256]
-            loss,_ = model(content, style)
+            loss_c, loss_s, _ = model(content, style)
+            loss = loss_c + 10 * loss_s
 
             optimizer.zero_grad()
-            loss.mean().backward()
+            loss.backward()
             optimizer.step()
             print(
-                f'[{e}/{opt.epoch} epoch],[{(e-1)*iters+i} /'f'{round(iters/opt.batch_size)}]: {loss.mean().item()}')
+                f'[{e}/{opt.epoch} epoch],[{i} /'f'{round(iters/opt.batch_size)}]: {loss_c.item()} and {loss_s.item()}')
 
             if i % opt.loss_interval == 0:
-                vis.line(Y=np.array([loss.mean().item()]), X=np.array(
-                    [(e-1)*iters+i]), win='loss', update='append')
+                vis.line(Y=np.array([loss_c.item()]), X=np.array(
+                    [(e-1)*round(iters/opt.batch_size)+i]), win='loss_c', update='append', opts=dict(xlabel='iteration',
+                                                                                                     ylabel='Content loss',
+                                                                                                     title='loss_c',
+                                                                                                     legend=['Loss']))
+                vis.line(Y=np.array([loss_s.item()]), X=np.array(
+                    [(e-1)*round(iters/opt.batch_size)+i]), win='loss_s', update='append', opts=dict(xlabel='iteration',
+                                                                                                     ylabel='style loss',
+                                                                                                     title='loss_s',
+                                                                                                     legend=['Loss']))
+                vis.line(Y=np.array([loss.item()]), X=np.array(
+                    [(e-1)*round(iters/opt.batch_size)+i]), win='loss', update='append', opts=dict(xlabel='iteration',
+                                                                                                     ylabel='Total loss',
+                                                                                                     title='loss',
+                                                                                                     legend=['Loss']))
 
             if i % opt.img_interval == 0:
                 c, s = next(test_iter)
                 c = c.to(device)
                 s = s.to(device)
                 with torch.no_grad():
-                    _, out = model(c, s)
+                    out = model.generate(c, s)
                 c = denorm(c, device)
                 s = denorm(s, device)
                 out = denorm(out, device)
                 res = torch.cat([c, s, out], dim=0)
-                vis.images(torch.clamp(res, 0, 1), win='image', nrow=opt.test_bs)
-
-        # ipdb.set_trace()
+                vis.images(torch.clamp(res, 0, 1),
+                           win='image', nrow=opt.test_bs)
 
 
 if __name__ == '__main__':
